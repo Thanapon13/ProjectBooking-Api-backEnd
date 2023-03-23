7cf1d893c6e4065f5a1344a64d51759c7ad7060d
DESKTOP-9MK91S8\admin <harukasenpais@gmail.com>
2023-03-24 00:10:35 +0700

    pnpm add chalk

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-const { sequelize } = require("./models");
-sequelize.sync({ force: true });
-// sequelize.sync({ alter: true });
+// const { sequelize } = require("./models");
+// sequelize.sync({ force: true });
+// // sequelize.sync({ alter: true });
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server running on port: ${port}`));
